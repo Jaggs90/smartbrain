3308dbe05dee0794f03af9079b157063f7b84ec7
Jaggs90 <djaggs@gmail.com>
2020-06-18 21:08:16 +0100

    added autoComplete to forms

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -60,13 +60,15 @@ class Register extends React.Component {
             </div>
             <div className="mt3">
           <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
-          <input
-           className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" 
-           type="email" autoComplete="email"
-           name="email-address"  
-           id="email-address"
+          <input 
+           className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+           type="email"  
+           autoComplete="email" placeholder="Enter your email"
+           name="email"  
+           id="email"
              onChange={this.onEmailChange}
            />
+           
         </div><form>
             <div className="mv3">
               <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
@@ -87,7 +89,7 @@ class Register extends React.Component {
              value="Register"
             />
           </div>
-        </div>
+          </div>
       </main>
       </article>
     );
